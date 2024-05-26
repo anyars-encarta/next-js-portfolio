780b8d825ea6040d5187eba521adaded639744bb
anyars-encarta <anyarsencarta@gmail.com>
2024-05-26 17:51:25 +0000

    Set basic steps

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        black: {
+          DEFAULT: '#000',
+          100: '#000319'
+        }
+      }
     },
   },
   plugins: [],
