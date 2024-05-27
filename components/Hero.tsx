501dd3942690a6d03a04501dae6ecefe3df38acb
anyars-encarta <anyarsencarta@gmail.com>
2024-05-27 07:22:43 +0000

    Add aceternity Magic Button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import MagicButton from './ui/MagicButton'
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
@@ -29,6 +30,10 @@ const Hero = () => {
                     <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
                         Hi, I&apos;m Anyars, a Full-stack Developer based in Ghana, West Africa.
                     </p>
+
+                    <a href="#about">
+                        <MagicButton title='Show my work' />
+                    </a>
                 </div>
             </div>
         </div>
