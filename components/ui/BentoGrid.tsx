1c27919ad180a7adf077e0222c4c8585000d094f
anyars-encarta <anyarsencarta@gmail.com>
2024-05-27 14:52:37 +0000

    Add grid globe to card

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import { GlobeDemo } from "./GridGlobe";
 
 export const BentoGrid = ({
     className,
@@ -42,7 +43,7 @@ export const BentoGridItem = ({
     return (
         <div
             className={cn(
-                "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 ",
+                "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 ",
                 className
             )}
             style={{
@@ -51,7 +52,7 @@ export const BentoGridItem = ({
             }}
         >
 
-            <div className={`${id === 6} && 'flex justify-center h-full'`}>
+            <div className={`${id === 6} && 'flex justify-center h-full absolute'`}>
                 <div className='w-full h-full absolute'>
                     {img && (
                         <img
@@ -90,6 +91,8 @@ export const BentoGridItem = ({
                         {title}
                     </div>
                 </div>
+
+                {id === 2 && <GlobeDemo />}
             </div>
         </div>
     );
