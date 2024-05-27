a82483e9a6bc2400f843ab873b9e9eb8ad48c9b8
anyars-encarta <anyarsencarta@gmail.com>
2024-05-27 08:15:11 +0000

    Correct Hero text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,7 +28,7 @@ const Hero = () => {
 
                     <TextGenerateEffect
                         className='text-center text-40px md:text-5xl lg:text-6xl'
-                        words='Transforming Concepts into Seamless Experiences'
+                        words='Transforming Concepts into Seamless User Experiences'
                     />
 
                     <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
