f64d6d249863caf74002853a9ec0110aa91adb7f
anyars-encarta <anyarsencarta@gmail.com>
2024-05-28 12:01:46 +0000

    Complete projects section

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,7 +1,7 @@
 import { projects } from "@/data";
 import React from "react";
 import { PinContainer } from "./ui/3d-pin";
-import { FaLocationArrow } from "react-icons/fa6";
+import { FaGithub, FaLocationArrow } from "react-icons/fa6";
 
 const RecentProjects = () => {
     return (
@@ -12,7 +12,7 @@ const RecentProjects = () => {
             </h1>
             <div className='flex flex-wrap items-center justify-center p-4 gap-16 mt-10'>
                 {projects.map(({ id, title, des, img, iconLists, source, live }) => (
-                    <div key={id} className='lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]'>
+                    <div key={id} className='lg:min-h-[32.5rem] h-[25rem] flex flex-col items-center justify-between sm:w-96 w-[80vw]'>
                         <PinContainer title={live} href={live}>
                             <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                                 <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
@@ -53,6 +53,7 @@ const RecentProjects = () => {
                                 </div>
                             </div>
                         </PinContainer>
+                        <FaGithub />
                     </div>
                 ))}
             </div>
