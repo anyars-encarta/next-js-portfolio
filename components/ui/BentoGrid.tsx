b09029fd0a737d2380b44b03a7383a3bee1b24db
anyars-encarta <anyarsencarta@gmail.com>
2024-05-28 09:50:36 +0000

    Correct BentoGrid component

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -90,11 +90,7 @@ export const BentoGridItem = ({
                     )}
                 </div>
 
-                {id === 6 && (
-                    <BackgroundGradientAnimation>
-                        <div className='absolute z-50 flex items-center justify-center text-white font-bold ' />
-                    </BackgroundGradientAnimation>
-                )}
+                {id === 6 && <BackgroundGradientAnimation />}
 
                 <div className={cn(
                     titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
