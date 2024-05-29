109108fdbdfb51fdd61c2635462441d96f88ea17
anyars-encarta <anyarsencarta@gmail.com>
2024-05-29 11:34:15 +0000

    Add infinite moving cards

--- components/ClientsTestimonials.tsx
+++ components/ClientsTestimonials.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { InfiniteMovingCards } from "./ui/InfiniteMovingCards";
+import { testimonials } from '../data/index';
 
 const ClientsTestimonials = () => {
   return (
     <div id='testimonials' className='py-20'>
       <h1 className='heading'>
-        Kind words from {' '}
-        <span className='text-purple'>satisfied clients</span>
+        Kind words from
+        <span className='text-purple'> satisfied clients</span>
       </h1>
-      <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10'>
-
+      <div className='flex flex-col items-center'>
+        <div className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center relative overflow-hidden">
+          <InfiniteMovingCards
+            items={testimonials}
+            direction="right"
+            speed="slow"
+          />
+        </div>
       </div>
     </div>
   )
--- data/index.ts
+++ data/index.ts
@@ -200,30 +200,35 @@ export const testimonials = [
             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
         name: "Michael Johnson",
         title: "Director of AlphaStream Technologies",
+        img: "/profiles/MasterDP.jpg",
     },
     {
         quote:
             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
         name: "Michael Johnson",
         title: "Director of AlphaStream Technologies",
+        img: "/profiles/MasterDP.jpg",
     },
     {
         quote:
             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
         name: "Michael Johnson",
         title: "Director of AlphaStream Technologies",
+        img: "/profiles/MasterDP.jpg",
     },
     {
         quote:
             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
         name: "Michael Johnson",
         title: "Director of AlphaStream Technologies",
+        img: "/profiles/MasterDP.jpg",
     },
     {
         quote:
             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
         name: "Michael Johnson",
         title: "Director of AlphaStream Technologies",
+        img: "/profiles/MasterDP.jpg",
     },
 ];
 
