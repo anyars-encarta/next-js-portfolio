23ff26c7529cc9d5fa6f67560d77b720cae56f88
anyars-encarta <anyarsencarta@gmail.com>
2024-05-29 11:43:58 +0000

    Correct testimonials title styling

--- components/ClientsTestimonials.tsx
+++ components/ClientsTestimonials.tsx
@@ -9,14 +9,12 @@ const ClientsTestimonials = () => {
         Kind words from
         <span className='text-purple'> satisfied clients</span>
       </h1>
-      <div className='flex flex-col items-center'>
-        <div className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center relative overflow-hidden">
+      <div className='flex flex-col items-center mt-10'>
           <InfiniteMovingCards
             items={testimonials}
             direction="right"
             speed="slow"
           />
-        </div>
       </div>
     </div>
   )
