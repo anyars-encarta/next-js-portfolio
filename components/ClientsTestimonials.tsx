2e17401f47a2c15b78db1ddf8f175aae86d70df3
anyars-encarta <anyarsencarta@gmail.com>
2024-05-29 10:15:50 +0000

    Trying to add hover effect on tech stack icons to display details

--- components/ClientsTestimonials.tsx
+++ components/ClientsTestimonials.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const ClientsTestimonials = () => {
   return (
-    <div id='testimonials'>ClientsTestimonials</div>
+    <div id='testimonials' className='py-20'>
+      <h1 className='heading'>
+        Kind words from {' '}
+        <span className='text-purple'>satisfied clients</span>
+      </h1>
+      <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10'>
+
+      </div>
+    </div>
   )
 }
 
