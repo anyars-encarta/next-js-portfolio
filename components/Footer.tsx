61e0e8add11ad6ed07b6e5c4483f87f30e3c6936
anyars-encarta <anyarsencarta@gmail.com>
2024-05-29 14:56:59 +0000

    Complete footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import MagicButton from './ui/MagicButton'
 import { FaLocationArrow } from 'react-icons/fa6';
+import { socialMedia } from '@/data';
+import { div } from 'three/examples/jsm/nodes/Nodes.js';
 
 const Footer = () => {
-    const handleCopy = () => {
-        navigator.clipboard.writeText('anyarsencarta@gmail.com');
-
-        setCopied(true);
-    };
+    const date = new Date();
+    const year = date.getFullYear();
 
     return (
         <footer id='contact' className='w-full pt-20 pb-10'>
@@ -30,6 +29,20 @@ const Footer = () => {
                         position='right'
                     />
                 </a>
+            </div>
+
+            <div className='flex mt-16 md:flex-row flex-col justify-between items-center'>
+                <p className='md:text-base text-sm md:font-normal font-light'>
+                    Copyright © {year} Anyars
+                </p>
+
+                <div className='flex items-center md:gap-3 gap-6'>
+                    {socialMedia.map((social) => (
+                        <div key={social.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
+                            <a href={social.link} target='_blank'><img src={social.img} alt={social.img} width={20} height={20}/></a>
+                        </div>
+                    ))}
+                </div>
             </div>
         </footer>
     )
