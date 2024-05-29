5445abe5fafd86960ff8b5b25da493e12c739beb
anyars-encarta <anyarsencarta@gmail.com>
2024-05-29 14:17:10 +0000

    Add footer heading and paragraph texts

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <footer id='contact' className='w-full pt-20 pb-10'>
-        <div>
-            <img src='/footer-grid.svg' alt='grid' className='w-full h-full opacity-50' />
-        </div>
-    </footer>
-  )
+    return (
+        <footer id='contact' className='w-full pt-20 pb-10'>
+            <div className='w-full absolute left-0 -bottom-72 min-h-96'>
+                <img src='/footer-grid.svg' alt='grid' className='w-full h-full opacity-50' />
+            </div>
+
+            <div className='flex flex-col items-center'>
+                <h1 className='heading lg:max-w-45[vw]'>
+                    Ready to take <span className='text-purple'>your</span> digital presence to the next level?
+                </h1>
+                <p className='text-white-200 md:mt-10 my-5 text-center'>
+                    Reach out to me today and let&apos;s discuss how I can help you achieve your goals.
+                </p>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
