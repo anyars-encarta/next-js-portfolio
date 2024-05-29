29bff62fd7694b82f81f915bcad960b886d85320
anyars-encarta <anyarsencarta@gmail.com>
2024-05-29 12:42:16 +0000

    Complete experience section

--- components/Experiences.tsx
+++ components/Experiences.tsx
@@ -17,11 +17,22 @@ const Experiences = () => {
                         borderRadius="1.75rem"
                         className="flex-1 text-white border-neutral-200 dark:border-slate-800"
                     >
-                        <img
-                            src={experience.thumbnail}
-                            alt={experience.title}
-                            className='lg:w-32 md:w-20 w-16'
-                        />
+                        <div className='flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2'>
+                            <img
+                                src={experience.thumbnail}
+                                alt={experience.title}
+                                className='lg:w-32 md:w-20 w-16'
+                            />
+
+                            <div className='lg:ms-5'>
+                                <h1 className='text-start text-xl md:text-2xl font-bold'>
+                                    {experience.title}
+                                </h1>
+                                <p className='text-start text-white-100 mt-3 font-semibold'>
+                                    {experience.desc}
+                                </p>
+                            </div>
+                        </div>
                     </Button>
                 ))}
             </div>
