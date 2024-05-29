79ebc470eb4651566504e824c2e01b3f130a1cdb
anyars-encarta <anyarsencarta@gmail.com>
2024-05-29 11:54:56 +0000

    Complete testimonials

--- components/ClientsTestimonials.tsx
+++ components/ClientsTestimonials.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { InfiniteMovingCards } from "./ui/InfiniteMovingCards";
-import { testimonials } from '../data/index';
+import { companies, testimonials } from '../data/index';
 
 const ClientsTestimonials = () => {
   return (
@@ -10,11 +10,32 @@ const ClientsTestimonials = () => {
         <span className='text-purple'> satisfied clients</span>
       </h1>
       <div className='flex flex-col items-center mt-10'>
-          <InfiniteMovingCards
-            items={testimonials}
-            direction="right"
-            speed="slow"
-          />
+        <InfiniteMovingCards
+          items={testimonials}
+          direction="right"
+          speed="slow"
+        />
+
+        <div className='flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10'>
+          {companies.map((company) => (
+            <div
+              key={company.id}
+              className='flex md:max-w-60 max-w-32 gap-2'
+            >
+              <img
+                src={company.img}
+                alt={company.name}
+                className='md:w-10 w-5'
+              />
+
+              <img
+                src={company.nameImg}
+                alt={company.name}
+                className='md:w-24 w-20'
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
