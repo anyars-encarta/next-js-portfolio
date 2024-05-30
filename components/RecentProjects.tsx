328f6a434a12efe2a6dfc0979cde2e23ea02bd1c
anyars-encarta <anyarsencarta@gmail.com>
2024-05-30 08:17:29 +0000

    Add magic border button around Github icon

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -61,10 +61,15 @@ const RecentProjects = () => {
                                 </div>
                             </a>
                         </PinContainer>
+                        <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+                            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 p-2 text-sm font-medium text-white backdrop-blur-3xl">
+                                <a href={source} target='_blank'>
+                                    <FaGithub className='w-8 h-8' />
+                                </a>
+                            </span>
+                        </button>
 
-                        <a href={source} target='_blank'>
-                            <FaGithub className='w-6 h-6 opacity-0 group-hover:opacity-100 transition-opacity duration-300' />
-                        </a>
                     </div>
                 ))}
             </div>
