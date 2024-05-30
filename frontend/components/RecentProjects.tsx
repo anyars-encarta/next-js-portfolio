6344fa638cd4ff0d4737def357df37d6f56b02e5
anyars-encarta <anyarsencarta@gmail.com>
2024-05-30 19:04:39 +0000

    Complete Projects section data

--- frontend/components/RecentProjects.tsx
+++ frontend/components/RecentProjects.tsx
@@ -17,7 +17,6 @@ const RecentProjects = () => {
           });
       }, []);
 
-      console.log(projects);
     return (
         <div id='projects' className='py-20'>
             <h1 className='heading'>
@@ -25,30 +24,30 @@ const RecentProjects = () => {
                 <span className='text-purple'>recent projects</span>
             </h1>
             <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10'>
-                {projects.map(({ _id, title, description, imgUrl, tags, codeLink, projectLink }) => (
-                    <div key={_id} className='sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex flex-col items-center justify-evenly sm:w-[570px] w-[80vw] group'>
+                {projects.map((project) => (
+                    <div key={project._id} className='sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex flex-col items-center justify-evenly sm:w-[570px] w-[80vw] group'>
 
-                        <PinContainer title={projectLink} href={projectLink}>
-                            <a href={projectLink} target='_blank'>
+                        <PinContainer title={project.projectLink} href={project.projectLink}>
+                            <a href={project.projectLink} target='_blank'>
                                 <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sh:h-[40vh] h-[30vh] mb-10">
                                     <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                                         <img src='/bg.png' alt='bg-img' />
                                     </div>
                                     <img
-                                        src={urlFor(imgUrl)}
-                                        alt={title}
+                                        src={urlFor(project.imgUrl)}
+                                        alt={project.title}
                                         className='z-10 absolute bottom-0'
                                     />
                                 </div>
 
-                                <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>{title}</h1>
+                                <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>{project.title}</h1>
                                 <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2'>
-                                    {description}
+                                    {project.description}
                                 </p>
 
                                 <div className='flex items-center justify-between mt-7 mb-3'>
                                     <div className='flex items-center'>
-                                        {tags.map((tag, i) => (
+                                        {project.icons?.map((icon, i) => (
                                             <div
                                                 key={i}
                                                 className='relative group'
@@ -56,8 +55,8 @@ const RecentProjects = () => {
                                             >
                                                 <div className='border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex items-center justify-center'>
                                                     <img
-                                                        src={tag.img}
-                                                        alt={tag.label}
+                                                        src={urlFor(icon.img) || ''}
+                                                        alt={icon.stack || ''}
                                                         className='p-2'
                                                     />
                                                 </div>
@@ -86,7 +85,7 @@ const RecentProjects = () => {
 
                         {/* Button code */}
                         <button className="inline-flex h-10 w-10 animate-shimmer items-center justify-center rounded-full border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 opacity-0 group-hover:opacity-100 transition-opacity group-hover:transition-colors duration-300">
-                            <a href={codeLink} target='_blank'>
+                            <a href={project.codeLink} target='_blank'>
                                 <FaGithub className='w-8 h-8' />
                             </a>
                         </button>
